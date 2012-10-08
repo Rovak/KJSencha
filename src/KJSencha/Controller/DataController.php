72fc7fc1d6727deac549ad0650f99e25958d2537
Roy van Kaathoven <royvankaathoven@hotmail.com>
2012-10-08 22:15:03 +0200

    Create ErrorPanel on error

--- src/KJSencha/Controller/DataController.php
+++ src/KJSencha/Controller/DataController.php
@@ -2,18 +2,42 @@
 
 namespace KJSencha\Controller;
 
+use Exception;
+use KJSencha\Frontend as Ext;
 use Zend\Mvc\Controller\AbstractActionController;
 
 class DataController extends AbstractActionController
 {
+    /**
+     * Component builder
+     * 
+     * Fetches components from the ComponentManager and converts
+     * them to a JSON format
+     * 
+     * The output can be used by Ext.ComponentLoader
+     * 
+     * @return \Zend\Http\Response
+     * @throws Exception
+     */
     public function componentAction()
     {
+        $response = $this->getResponse();
         $sm = $this->getServiceLocator();
         $componentManager = $sm->get('kjsencha.cmpmgr');
 
-        $component = $componentManager->get($this->params()->fromPost('className'));
+        try {
+            $component = $componentManager->get($this->params()->fromPost('className'));
+            throw new Exception('test');
+        } catch(Exception $e) {
+            // When something goes wrong create a new panel which holds the error message
+            $component = new Ext\Panel(array(
+                'html'          => 'Exception: ' . $e->getMessage(),
+                'bodyPadding'   => 5,
+                'bodyStyle'     => 'color: #F00; text-align: center',
+                'border'        => 0,
+            ));
+        }
 
-        $response = $this->getResponse();
         $response->setContent($component->toJson());
 
         return $response;
