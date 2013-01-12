27f4f5a560be5163eabf341412f030956f31d495
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-01-12 15:10:37 +0100

    Unused namespace

--- src/KJSencha/Service/ComponentManagerFactory.php
+++ src/KJSencha/Service/ComponentManagerFactory.php
@@ -2,7 +2,6 @@
 
 namespace KJSencha\Service;
 
-use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\Mvc\Service\ServiceManagerConfig;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
