71ae7a73a71ef81f7202c4a086ff97392c0d4d29
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-02-03 20:30:50 +0100

    Added service tests

--- tests/KJSenchaTest/Annotation/FormhandlerTest.php
+++ tests/KJSenchaTest/Annotation/FormhandlerTest.php
@@ -7,18 +7,19 @@ use PHPUnit_Framework_TestCase;
 
 class FormhandlerTest extends PHPUnit_Framework_TestCase
 {
+
     /**
      * @var \KJSencha\Direct\Remoting\Api\Factory\ApiBuilder
      */
     protected $apiBuilder;
-    
+
     public function setUp()
     {
         $sl = ServiceManagerFactory::getServiceManager();
-        
+
         $this->apiBuilder = $sl->get('kjsencha.apibuilder');
     }
-    
+
     /**
      * @covers KJSencha\Annotation\Formhandler::decorateObject
      */
@@ -26,7 +27,7 @@ class FormhandlerTest extends PHPUnit_Framework_TestCase
     {
         /* @var $action \KJSencha\Direct\Remoting\Api\Object\Action */
         $action = $this->apiBuilder->buildAction('KJSenchaTestAsset\Direct\Form\Profile');
-        
+
         $this->assertTrue($action->hasMethod('updateBasicInfo'));
         $this->assertTrue($action->getMethod('updateBasicInfo')->getOption('formHandler'));
     }
@@ -42,4 +43,43 @@ class FormhandlerTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($action->hasMethod('getBasicInfo'));
         $this->assertNull($action->getMethod('getBasicInfo')->getOption('formHandler'));
     }
+
+    /**
+     * @covers KJSencha\Annotation\Formhandler::decorateObject
+     */
+    public function testServiceAnnotationDecoratesMethod()
+    {
+        /* @var $api \KJSencha\Direct\Remoting\Api\Api */
+        $api = $this->apiBuilder->buildApi(array(
+            'services' => array(
+                'Direct.Profile' => 'KJSenchaTestAsset\Direct\Form\Profile',
+            )
+        ));
+
+        /* @var $action \KJSencha\Direct\Remoting\Api\Object\Action */
+        $action = $api->getAction('Direct.Profile');
+
+        $this->assertTrue($action->hasMethod('updateBasicInfo'));
+        $this->assertTrue($action->getMethod('updateBasicInfo')->getOption('formHandler'));
+    }
+
+    /**
+     * @covers KJSencha\Annotation\Formhandler::decorateObject
+     */
+    public function testServiceAnnotationDoesNotDecorateOthers()
+    {
+        /* @var $api \KJSencha\Direct\Remoting\Api\Api */
+        $api = $this->apiBuilder->buildApi(array(
+            'services' => array(
+                'Direct.Profile' => 'KJSenchaTestAsset\Direct\Form\Profile',
+            )
+        ));
+
+        /* @var $action \KJSencha\Direct\Remoting\Api\Object\Action */
+        $action = $api->getAction('Direct.Profile');
+
+        $this->assertTrue($action->hasMethod('getBasicInfo'));
+        $this->assertNull($action->getMethod('getBasicInfo')->getOption('formHandler'));
+    }
+
 }
