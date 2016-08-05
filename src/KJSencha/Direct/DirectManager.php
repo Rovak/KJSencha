43cfd6befe55d2af1fa7bac44622b4b307c056b8
4orever <riv82girsul>
2016-08-05 17:25:57 +0300

    DirectManager compatibility with ZF 3.0

--- src/KJSencha/Direct/DirectManager.php
+++ src/KJSencha/Direct/DirectManager.php
@@ -3,12 +3,24 @@
 namespace KJSencha\Direct;
 
 use Zend\ServiceManager\AbstractPluginManager;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\ServiceManager\ServiceManager;
 
 /**
  * Manages the creation of Direct classes
  */
 class DirectManager extends AbstractPluginManager
 {
+    /**
+     * DirectManager constructor.
+     * @param ServiceLocatorInterface $sl
+     */
+    public function __construct(ServiceLocatorInterface $sl)
+    {
+        parent::__construct();
+        $this->setServiceLocator($sl);
+    }
+
     /**
      * {@inheritDoc}
      */
